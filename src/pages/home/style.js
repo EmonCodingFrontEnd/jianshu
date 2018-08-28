6f77d479d2f4931f4b72020b6e4b934699160317
Rushing <liming20110711@163.com>
2018-08-28 08:34:37 +0800

    update:20180828-02

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -19,3 +19,26 @@ export const HomeRight = styled.div`
   float: right;
   width: 240px;
 `;
+
+export const TopicWrapper = styled.div`
+  overflow: hidden;
+  padding: 20px 0 10px 0;
+`;
+export const TopicItem = styled.div`
+  float: left;
+  background: #f7f7f7;
+  height: 32px;
+  line-height: 32px;
+  padding-right: 10px;
+  font-size: 14px;
+  color: #000;
+  border: 1px solid #dcdcdc;
+  border-radius: 4px;
+  .topic-pic {
+    display: block;
+    float: left;
+    width: 32px;
+    height: 32px;
+    margin-right: 10px;
+  }
+`;
